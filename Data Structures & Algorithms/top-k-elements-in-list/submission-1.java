c3ea410d0b57453c2e8ac5bfb63c94e41f32fb62
vvk-vivekgupta <vvk.vivekgupta@gmail.com>
2026-07-05 21:04:01 +0530

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        HashSet<Integer>[] rank = new HashSet[nums.length+1];
+        rank[0] = new HashSet<>();
+
+        for (int n : nums) {
+            // Update freq of each number
+            freq.put(n, freq.getOrDefault(n, 0) + 1);
+            int count = freq.get(n);
+
+            if (rank[count] == null)
+                rank[count] = new HashSet<>();
+
+            // Reverse map the freq with the num
+            // And remove the old freq of that num
+            rank[count].add(n);
+            rank[count-1].remove(n);
+        }
+
+        int res[] = new int[k];
+        int j = 0;
+        // Loop through the rank, and get the top k
+        for (int i = nums.length; i >= 0; i--) {
+            if (rank[i] != null) {
+                for (var n : rank[i]) {
+                    res[j] = n;
+                    j++;
+                    if (j >= k)
+                        return res;
+                }
+            }
+        }
+
+        return res;
+    }
+}
